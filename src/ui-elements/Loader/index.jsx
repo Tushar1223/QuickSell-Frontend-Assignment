fdc3cc3989cfdaedb74262a7843e57182143fc93
Tushar Sharma <tushar007king@gmail.com>
2024-10-29 13:45:50 +0530

    Loader Redesigning

--- src/ui-elements/Loader/index.jsx
+++ src/ui-elements/Loader/index.jsx
@@ -4,7 +4,11 @@ import './loader.css';
 function Loader({ fullscreen = true }) {
     return (
         <div className={`loader-container ${fullscreen ? "fullscreen" : ""}`}>
-            <span className='loader'>Loading...</span>
+            <div className="rl-loading-container">
+                <div className="rl-loading-thumb rl-loading-thumb-1"></div>
+                <div className="rl-loading-thumb rl-loading-thumb-2"></div>
+                <div className="rl-loading-thumb rl-loading-thumb-3"></div>
+            </div>
         </div>
     );
 }
